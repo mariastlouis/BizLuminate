b6e8edee16bd1ee24c698c7e6c393b15df974f7d
Thomas Laird <t6r6l5@gmail.com>
2019-03-12 17:11:56 -0600

    add boilerplate server files

---
+++ test/routes.spec.js
@@ -0,0 +1,33 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const knex = require('../db/config');
+
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+describe('Server tests', () => {
+  beforeEach(done => {
+    // knex.seed.run()
+      // .then( () => {
+        done();
+      // });
+  });
+
+  it('Should get from the test route', () => {
+    return chai.request(server)
+      .get('/api/v1/test')
+      .then(res => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.have.property('status');
+        res.body.status.should.equal('success');
+      })
+      .catch(err => {
+        throw err;
+      });
+  });
+});
