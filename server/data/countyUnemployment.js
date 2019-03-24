6d71476bf1f18446858b0daeb6fa73cfa3c483a0
Maria St. Louis-Sanchez <msantray@yahoo.com>
2019-03-23 21:52:03 -0600

    added county unemployment, transportation to work, and general demographics

---
+++ server/data/countyUnemployment.js
@@ -0,0 +1,386 @@
+[
+  {
+    "countyId": 8001,
+    "countyName": "Adams County",
+    "Rank": 25,
+    "UnemploymentRate": "3.80%"
+  },
+  {
+    "countyId": 8003,
+    "countyName": "Alamosa County",
+    "Rank": 14,
+    "UnemploymentRate": "4.90%"
+  },
+  {
+    "countyId": 8005,
+    "countyName": "Arapahoe County",
+    "Rank": 33,
+    "UnemploymentRate": "3.30%"
+  },
+  {
+    "countyId": 8007,
+    "countyName": "Archuleta County",
+    "Rank": 19,
+    "UnemploymentRate": "4.40%"
+  },
+  {
+    "countyId": 8009,
+    "countyName": "Baca County",
+    "Rank": 56,
+    "UnemploymentRate": "2.30%"
+  },
+  {
+    "countyId": 8011,
+    "countyName": "Bent County",
+    "Rank": 20,
+    "UnemploymentRate": "4.20%"
+  },
+  {
+    "countyId": 8013,
+    "countyName": "Boulder County",
+    "Rank": 42,
+    "UnemploymentRate": "2.90%"
+  },
+  {
+    "countyId": 8014,
+    "countyName": "Broomfield County",
+    "Rank": 41,
+    "UnemploymentRate": "3.00%"
+  },
+  {
+    "countyId": 8015,
+    "countyName": "Chaffee County",
+    "Rank": 34,
+    "UnemploymentRate": "3.30%"
+  },
+  {
+    "countyId": 8017,
+    "countyName": "Cheyenne County",
+    "Rank": 63,
+    "UnemploymentRate": "2.00%"
+  },
+  {
+    "countyId": 8019,
+    "countyName": "Clear Creek County",
+    "Rank": 43,
+    "UnemploymentRate": "2.90%"
+  },
+  {
+    "countyId": 8021,
+    "countyName": "Conejos County",
+    "Rank": 8,
+    "UnemploymentRate": "5.70%"
+  },
+  {
+    "countyId": 8023,
+    "countyName": "Costilla County",
+    "Rank": 3,
+    "UnemploymentRate": "6.20%"
+  },
+  {
+    "countyId": 8025,
+    "countyName": "Crowley County",
+    "Rank": 9,
+    "UnemploymentRate": "5.60%"
+  },
+  {
+    "countyId": 8027,
+    "countyName": "Custer County",
+    "Rank": 29,
+    "UnemploymentRate": "3.50%"
+  },
+  {
+    "countyId": 8029,
+    "countyName": "Delta County",
+    "Rank": 15,
+    "UnemploymentRate": "4.70%"
+  },
+  {
+    "countyId": 8031,
+    "countyName": "Denver County",
+    "Rank": 31,
+    "UnemploymentRate": "3.40%"
+  },
+  {
+    "countyId": 8033,
+    "countyName": "Dolores County",
+    "Rank": 24,
+    "UnemploymentRate": "3.90%"
+  },
+  {
+    "countyId": 8035,
+    "countyName": "Douglas County",
+    "Rank": 44,
+    "UnemploymentRate": "2.90%"
+  },
+  {
+    "countyId": 8037,
+    "countyName": "Eagle County",
+    "Rank": 50,
+    "UnemploymentRate": "2.60%"
+  },
+  {
+    "countyId": 8039,
+    "countyName": "Elbert County",
+    "Rank": 45,
+    "UnemploymentRate": "2.90%"
+  },
+  {
+    "countyId": 8041,
+    "countyName": "El Paso County",
+    "Rank": 21,
+    "UnemploymentRate": "4.20%"
+  },
+  {
+    "countyId": 8043,
+    "countyName": "Fremont County",
+    "Rank": 4,
+    "UnemploymentRate": "6.20%"
+  },
+  {
+    "countyId": 8045,
+    "countyName": "Garfield County",
+    "Rank": 26,
+    "UnemploymentRate": "3.70%"
+  },
+  {
+    "countyId": 8047,
+    "countyName": "Gilpin County",
+    "Rank": 47,
+    "UnemploymentRate": "2.80%"
+  },
+  {
+    "countyId": 8049,
+    "countyName": "Grand County",
+    "Rank": 48,
+    "UnemploymentRate": "2.70%"
+  },
+  {
+    "countyId": 8051,
+    "countyName": "Gunnison County",
+    "Rank": 46,
+    "UnemploymentRate": "2.90%"
+  },
+  {
+    "countyId": 8053,
+    "countyName": "Hinsdale County",
+    "Rank": 36,
+    "UnemploymentRate": "3.20%"
+  },
+  {
+    "countyId": 8055,
+    "countyName": "Huerfano County",
+    "Rank": 1,
+    "UnemploymentRate": "8.90%"
+  },
+  {
+    "countyId": 8057,
+    "countyName": "Jackson County",
+    "Rank": 30,
+    "UnemploymentRate": "3.50%"
+  },
+  {
+    "countyId": 8059,
+    "countyName": "Jefferson County",
+    "Rank": 37,
+    "UnemploymentRate": "3.20%"
+  },
+  {
+    "countyId": 8061,
+    "countyName": "Kiowa County",
+    "Rank": 60,
+    "UnemploymentRate": "2.10%"
+  },
+  {
+    "countyId": 8063,
+    "countyName": "Kit Carson County",
+    "Rank": 58,
+    "UnemploymentRate": "2.20%"
+  },
+  {
+    "countyId": 8065,
+    "countyName": "Lake County",
+    "Rank": 51,
+    "UnemploymentRate": "2.60%"
+  },
+  {
+    "countyId": 8067,
+    "countyName": "La Plata County",
+    "Rank": 27,
+    "UnemploymentRate": "3.70%"
+  },
+  {
+    "countyId": 8069,
+    "countyName": "Larimer County",
+    "Rank": 40,
+    "UnemploymentRate": "3.10%"
+  },
+  {
+    "countyId": 8071,
+    "countyName": "Las Animas County",
+    "Rank": 10,
+    "UnemploymentRate": "5.20%"
+  },
+  {
+    "countyId": 8073,
+    "countyName": "Lincoln County",
+    "Rank": 49,
+    "UnemploymentRate": "2.70%"
+  },
+  {
+    "countyId": 8075,
+    "countyName": "Logan County",
+    "Rank": 38,
+    "UnemploymentRate": "3.20%"
+  },
+  {
+    "countyId": 8077,
+    "countyName": "Mesa County",
+    "Rank": 16,
+    "UnemploymentRate": "4.60%"
+  },
+  {
+    "countyId": 8079,
+    "countyName": "Mineral County",
+    "Rank": 61,
+    "UnemploymentRate": "2.10%"
+  },
+  {
+    "countyId": 8081,
+    "countyName": "Moffat County",
+    "Rank": 11,
+    "UnemploymentRate": "5.20%"
+  },
+  {
+    "countyId": 8083,
+    "countyName": "Montezuma County",
+    "Rank": 2,
+    "UnemploymentRate": "6.60%"
+  },
+  {
+    "countyId": 8085,
+    "countyName": "Montrose County",
+    "Rank": 17,
+    "UnemploymentRate": "4.60%"
+  },
+  {
+    "countyId": 8087,
+    "countyName": "Morgan County",
+    "Rank": 35,
+    "UnemploymentRate": "3.30%"
+  },
+  {
+    "countyId": 8089,
+    "countyName": "Otero County",
+    "Rank": 6,
+    "UnemploymentRate": "5.80%"
+  },
+  {
+    "countyId": 8091,
+    "countyName": "Ouray County",
+    "Rank": 22,
+    "UnemploymentRate": "4.10%"
+  },
+  {
+    "countyId": 8093,
+    "countyName": "Park County",
+    "Rank": 32,
+    "UnemploymentRate": "3.40%"
+  },
+  {
+    "countyId": 8095,
+    "countyName": "Phillips County",
+    "Rank": 62,
+    "UnemploymentRate": "2.10%"
+  },
+  {
+    "countyId": 8097,
+    "countyName": "Pitkin County",
+    "Rank": 52,
+    "UnemploymentRate": "2.60%"
+  },
+  {
+    "countyId": 8099,
+    "countyName": "Prowers County",
+    "Rank": 28,
+    "UnemploymentRate": "3.60%"
+  },
+  {
+    "countyId": 8101,
+    "countyName": "Pueblo County",
+    "Rank": 12,
+    "UnemploymentRate": "5.20%"
+  },
+  {
+    "countyId": 8103,
+    "countyName": "Rio Blanco County",
+    "Rank": 5,
+    "UnemploymentRate": "6.00%"
+  },
+  {
+    "countyId": 8105,
+    "countyName": "Rio Grande County",
+    "Rank": 7,
+    "UnemploymentRate": "5.80%"
+  },
+  {
+    "countyId": 8107,
+    "countyName": "Routt County",
+    "Rank": 53,
+    "UnemploymentRate": "2.60%"
+  },
+  {
+    "countyId": 8109,
+    "countyName": "Saguache County",
+    "Rank": 13,
+    "UnemploymentRate": "5.00%"
+  },
+  {
+    "countyId": 8111,
+    "countyName": "San Juan County",
+    "Rank": 18,
+    "UnemploymentRate": "4.60%"
+  },
+  {
+    "countyId": 8113,
+    "countyName": "San Miguel County",
+    "Rank": 55,
+    "UnemploymentRate": "2.40%"
+  },
+  {
+    "countyId": 8115,
+    "countyName": "Sedgwick County",
+    "Rank": 54,
+    "UnemploymentRate": "2.60%"
+  },
+  {
+    "countyId": 8117,
+    "countyName": "Summit County",
+    "Rank": 64,
+    "UnemploymentRate": "1.90%"
+  },
+  {
+    "countyId": 8119,
+    "countyName": "Teller County",
+    "Rank": 23,
+    "UnemploymentRate": "4.10%"
+  },
+  {
+    "countyId": 8121,
+    "countyName": "Washington County",
+    "Rank": 57,
+    "UnemploymentRate": "2.30%"
+  },
+  {
+    "countyId": 8123,
+    "countyName": "Weld County",
+    "Rank": 39,
+    "UnemploymentRate": "3.20%"
+  },
+  {
+    "countyId": 8125,
+    "countyName": "Yuma County",
+    "Rank": 59,
+    "UnemploymentRate": "2.20%"
+  }
+]
