effb6ce2abf4afd338f8b1bb878459d1ec1b9615
Maria St. Louis-Sanchez <msantray@yahoo.com>
2019-04-09 15:41:51 -0600

    added unemployment and median income maps

--- client/src/Map/Map.js
+++ client/src/Map/Map.js
@@ -2,31 +2,104 @@ import React, {Component} from 'react';
 import mapboxgl from 'mapbox-gl'
 import './Map.scss'
 import key from '../mapKey'
+import countydata from '../geographyData/countydata.json'
+// import {countyUnemploymentFetch} from '../Helper/helper';
+// import countydatasimple from '../geographyData/countydatasimple.json'
+// import data from '../geographyData/data.json'
 
 mapboxgl.accessToken = key
 
+const options = [{
+  name: "unemployment",
+  description: 'Unemployment by Colorado County',
+  property: 'unemploymentRate',
+  stops: [
+    [0, '#f8d5cc'],
+    [3, '#f4bfb6'],
+    [4, '#ee8f9a'],
+    [90, '#9f43d7']
+  ]
+}]
 
 class Map extends Component {
 
   constructor(props){
     super(props)
       this.state = {
-        lng: -104.9389,
-        lat: 39.2587,
-        zoom: 6
+        active: options[0]
       }
   }
 
-  componentDidMount(){
-    const { lng, lat, zoom } = this.state;
-    const map = new mapboxgl.Map({
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: 'mapbox://styles/msantra/cjts4mlvt1es21fp70le4t23j',
-      center: [lng, lat],
-      zoom
+      style: 'mapbox://styles/msantra/cju9br68i4r8w1gnq67cec8xs',
+      center: [-106.110198, 37.751359 ],
+      zoom: 6
     });
-  }
 
+    this.map.on('load', () => {
+      console.log(this.map.getStyle().sources)
+      console.log(this.map.style)
+      // this.map.addSource('counties', {
+      //   type: 'geojson',
+      //   data: countydata
+      // })
+
+      // this.map.addSource('counties', {
+      //   type: 'vector',
+      //   url: 'mapbox://msantra.7x1t01n6'
+      // })
+
+      console.log(this.map.getStyle().sources)
+      console.log(this.map.style)
+      // this.map.addLayer({
+      //   'id': 'countyData',
+      //   'type': 'fill',
+      //   'source': 'counties',
+      //   'paint':{
+      //     'fill-color':'#f8d5cc',
+
+
+      //   }
+      // },'background');
+
+    })
+  }
+    // setFill() {
+    //   const {property, stops} = this.state.active;
+    //   this.map.setPaintProperty('counties', 'fill-color', {
+    //     property,
+    //     stops
+    //   });
+    //   console.log(this.map.style)
+    // }
+    // this.map.on('load', () => {
+    //   this.map.addSource('counties', {
+    //     type: 'vector',
+    //     url: 'mapbox://countyMapData-9pozcu'
+    //   });
+
+
+      // this.map.addLayer({
+      //   id: 'counties',
+      //   type: 'fill',
+      //   source: 'countyMapData-9pozcu',
+      //   paint: {
+      //     "fill-color": '#9f43d7'
+      //   }
+      // }, 'country-label-lg');
+
+    // })
+
+
+  // setFill() {
+  //   const { property, stops } = this.state.active;
+  //   this.map.setPaintProperty('countries', 'fill-color', {
+  //     property,
+  //     stops
+  //   });
+  // }
 
    render () {
     return (
