a4f84294e2db1ed57248544102ea34868549ef83
Maria St. Louis-Sanchez <msantray@yahoo.com>
2019-04-10 09:12:19 -0600

    client side data loading and filling

--- client/src/Map/Map.js
+++ client/src/Map/Map.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import mapboxgl from 'mapbox-gl'
 import './Map.scss'
 import key from '../mapKey'
+import countydata from '../geographyData/countydata.json';
 
 mapboxgl.accessToken = key
 
@@ -18,68 +19,73 @@ const mapLayers = [
   }
 ]
 
+const mapOptions = [
+  {
+    'name':'unemployment',
+    'property':'unemploymentRate',
+    'stops':[
+     [1.9, '#f0f9e8'],
+     [2.9, '#bae4bc'],
+     [3.9, '#7bccc4'],
+     [8.9, '#2b8cbe']
+    ]
+  }
+]
+
 class Map extends Component {
 
   constructor(props){
     super(props)
       this.state = {
-        activeLayer: mapLayers[0],
-        previousLayer: ''
+        activeLayer: mapOptions[0],
+        mapObj: {}
       }
   }
 
+  setFill(map) {
+    console.log(map)
+    const {property, stops} = this.state.activeLayer;
+    map.on('load', function() {
+      map.setPaintProperty('county-fill', 'fill-color',{
+        property,
+        stops
+      });
+    })
+
+  }
+
   componentDidMount() {
-     const map = new mapboxgl.Map({
+    const map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: 'mapbox://styles/msantra/cju9br68i4r8w1gnq67cec8xs',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      // style: 'mapbox://styles/msantra/cju9br68i4r8w1gnq67cec8xs',
       center: [-106.110198, 37.751359 ],
       zoom: 6
     });
 
-    console.log(map.style)
+    map.on('load', function() {
+   map.addSource('counties', {
+        'type': 'geojson',
+        'data': countydata
+      })
 
 
-     map.on('mousemove', function(e) {
-        let counties = map.queryRenderedFeatures(e.point);
-        if(counties.length > 0) {
-          document.getElementById('map-sidebar').innerHTML= '<h2>'+counties[0].properties.NAMELSAD10 + '</h2>'
-          console.log(counties[0].properties.NAMELSAD10)
-        }
-       });
+      map.addLayer({
+        'id': 'county-fill',
+        'type': 'fill',
+        'source': 'counties'
+      },'state-label');
+      })
 
-
-
-
-
+      // this.setState({mapObj: map})
+      // console.log(this.state.mapObj)
+      this.setFill(map);
 }
-    // this.map.on('load', () => {
-
-      // mapLayers.forEach((layer)=>{
-        // let input = document.createElement('input');
-        // input.type = 'checkbox';
-        // input.id = layer.id;
-        // input.checked = true;
-        // mapSidebar.appendChild('input')
-
-        // let label = document.createElement('label');
-        // label.setAttribute('for', layer.id);
-        // label.textContent = layer.name;
-        // mapSidebar.appendChild(label)
-
-      // })
-    // })
 
 
-  getCheckboxes = () => {
-    mapLayers.map((layer, index) => {
-      return (
-       <p>hello</p>
-        // <input type = "checkbox" name="layer-select" value = {layer.layerid} checked = {layer.checked}>
-        //   {layer.name}
-        // </input>
-      )
-    })
-  }
+
+
+
 
    render () {
     return (
